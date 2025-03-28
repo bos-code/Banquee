d4c569364051175cf72b49d9baaa19ace99e3e1b
Henry chidera <chidera9713@gmail.com>
2025-03-28 05:25:01 +0100

    feat: add SVG badge and improve navigation animation with responsive adjustments

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,4 @@
-console.log("animation");
-
-
-
-const navItems = gsap.utils.toArray(".nav_lists > *"); 
+const navItems = gsap.utils.toArray(".nav_lists > *");
 const navToggle = document.querySelector(".nav_toggle");
 const checkbox = document.querySelector("#checkbox");
 
@@ -15,58 +11,66 @@ gsap.registerPlugin(
 );
 
 document.querySelectorAll("section").forEach((section) => {
-    section.style.transformOrigin = "top center";
-    section.style.scale = 0.5;
-    section.style.opacity = 0;
+  section.style.transformOrigin = "top center";
+  section.style.scale = 0.5;
+  section.style.opacity = 0;
   ScrollTrigger.create({
     trigger: section,
     start: "top 70%",
     onEnter: () => {
-      gsap.to(section, { scale: 1,opacity:1, duration: .8 });
+      gsap.to(section, { scale: 1, opacity: 1, duration: 0.8 });
     },
     onLeaveBack: () => {
-      gsap.to(section, { scale: 0.5, opacity:0,duration: .8});
+      gsap.to(section, { scale: 0.5, opacity: 0, duration: 0.8 });
     },
   });
-
- 
 });
 
+const mm = gsap.matchMedia();
+mm.add("(min-width: 1024px)", () => {
+  const navAnimation = gsap.timeline({}).from(navItems, {
+    y: -30,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.15,
+    ease: "back",
+  });
 
-
-
-
-
-const navAnimation = gsap.timeline({})
-    .from(navItems, {
-        y: -30, 
-        opacity: 0,
-        duration: 0.6,
-        stagger: 0.15,
-        ease: "back",
-    });
-
-document.querySelectorAll("#a").forEach((item) => {
-    navAnimation.from(item, {
+  document.querySelectorAll("#a").forEach((item) => {
+    navAnimation.from(
+      item,
+      {
         y: -30,
         opacity: 0,
         duration: 0.6,
         stagger: 0.15,
         ease: "back",
-    }, "-=0.3");
+      },
+      "-=0.3"
+    );
+  });
 });
+mm.add("(max-width: 600px)", () => {
+  const navAnimation = gsap.timeline({}).from(navItems, {
+    xPercent: 0,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.15,
+    ease: "back",
+  });
 
-document.querySelectorAll(".nav_lists > *").forEach((item) => {
-    item.addEventListener("click", () => {
-        navAnimation.reverse();
-    });
+  document.querySelectorAll("#a").forEach((item) => {
+    navAnimation.from(
+      item,
+      {
+        xPercent: 0,
+
+        opacity: 0,
+        duration: 0.6,
+        stagger: 0.15,
+        ease: "back",
+      },
+      "-=0.3"
+    );
+  });
 });
-
-navToggle.addEventListener("click", () => {
-    if (checkbox.checked) {
-        navAnimation.play();
-    } else {
-        navAnimation.reverse();
-    } 
-});
-
